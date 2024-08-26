def51d49db44304054f0ecd474fbcefc79364bf8
TimiDev19 <rotimioluwatimilehin19@gmail.com>
2024-08-26 16:09:36 -0700

    student dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      
+    <div className=' flex items-center justify-center h-[100vh]'>
+      <h1 className=' text-center text-5xl'>Kwasu VTE</h1>
     </div>
   )
 }
