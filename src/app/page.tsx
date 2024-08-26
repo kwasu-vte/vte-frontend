27b77e2b4715ebfe77f571d78b34c8f5a3db4ab3
TimiDev19 <rotimioluwatimilehin19@gmail.com>
2024-08-25 22:25:45 -0700

    fresh start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,11 @@
-"use client";
-import Image from "next/image";
-import SignupPage from "../app/pages/signup/page";
-import { useEffect, useState } from "react";
+import React from 'react'
 
-export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false)
-    }, 4000)
-  })
-
+const page = () => {
   return (
-    <main className=" bg-white h-[100vh] w-[100vw]">
-      {
-        isLoading ? (
-          <div className="h-[100vh] w-[100vh] flex items-center justify-center name-reveal-container">
-            <div className=' h-[25px] w-[25px] md:h-[50px] md:w-[50px] mx-4 bg-gradient-to-tl from-green-400 to-green-100 rounded-full ball'></div>
-            <div className="name-reveal text-4xl md:text-7xl font-thin text-green-500 text-center">KWASU VTE</div>
-          </div>
-        ) : (
-          <SignupPage />
-        )
-      }
-
-    </main>
-  );
+    <div>
+      
+    </div>
+  )
 }
+
+export default page
