2f980691aedde0d488bb64edbc848410207a3301
Favour Idowu <favouridowu02@gmail.com>
2024-09-23 18:48:55 +0100

    Resolving the git merge errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,27 @@
 "use client";
 import React from 'react'
 import Dashboard from '@/app/studentDashboard/page'
-import { useMyProp } from './layout';
-import AdminDashboard from '@/app/adminDashboard/page'
 
-<<<<<<< HEAD
 interface PageProps {
   customProp: string;
 }
 
-const page = ({customProp}: PageProps): JSX.Element => {
-  console.log(`custom prop is ${customProp}`)
-  if (customProp === 'someValue') {
-=======
-const page = () => {
-  const myProp = useMyProp();
-  console.log(myProp)
 
-  if (myProp === "admin") {
->>>>>>> e2e800753b531902d0bd6ac7ed588b3e2218ed7b
+const page = (props) => {
+  console.log(`custom prop is ${props.customProp}`)
+  if (typeof props.customProp === 'string' && props.customProp === 'someValue') {
     return (
       <div>
-        <AdminDashboard />
+        fuck
       </div>
-    )
+    );
   } else {
     return (
       <div> 
         <Dashboard />
       </div>
-    )
+    );
   }
-
-
-
 }
 
 export default page
