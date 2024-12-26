e50728d2d05d099072bc48a0c721a951ad0b8f32
TimiDev19 <rotimioluwatimilehin19@gmail.com>
2024-12-26 14:47:01 +0100

    Student Mobile screens completed

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -14,6 +14,7 @@ import { Protected } from '@/components/protected'
 import Sidebar from '../components/Sidebar'
 import { useAuth } from '@/lib/auth'
 import { makePayment } from '@/lib/info'
+import ResponsiveSidebar from '../components/ResponsiveSidebar';
 
 const montserrat = Montserrat({
     subsets: ['latin'],
@@ -82,8 +83,8 @@ export default function Page() {
         <Protected>
             <Sidebar />
             <FadeInFromBottom>
-                <div className={` w-full h-[100vh] overflow-hidden pl-[20%] pb-5 py-2 pr-4`}>
-                    <div className=' h-[60px] w-full bg-transparent rounded-md flex items-center justify-between p-2 mb-4'>
+                <div className={` w-full min-h-[100vh] lg:overflow-hidden px-2 lg:px-0 lg:pl-[20%] pb-5 py-2 lg:pr-4`}>
+                    <div className=' h-[60px] w-full bg-transparent rounded-md hidden lg:flex items-center justify-between p-2 mb-4'>
                         <div className=' flex items-center justify-center bg-white p-2 rounded-md'>
                             <CalendarMonth className=' text-[#379E37]' />
                             <select name="" id="" className=' mx-4 appearance-none'>
@@ -102,15 +103,51 @@ export default function Page() {
                         </div>
                     </div>
 
-                    <div className=' w-full flex items-center justify-between'>
-                        <div className=' w-[60%]'>
+                    {/* Mobile header */}
+                    <div className=" lg:hidden w-[100vw] h-[60px] px-3 flex items-center justify-between">
+                        <ResponsiveSidebar />
+
+                        <div className=" h-[90%] w-fit bg-white rounded-lg px-2">
+                            <div className=" h-full flex items-center justify-center">
+                                <div className=" mx-2 bg-[#BFE7BF7A] h-[40px] w-[40px] rounded-md flex items-center justify-center p-2 cursor-pointer">
+                                    <Search />
+                                </div>
+
+                                <Link href={"/notifications/"}>
+                                    <Notifications className=" text-[#379E37] mr-3" />
+                                </Link>
+
+
+                                <div className=" mx-1 h-full flex flex-col items-start justify-center min-w-[100px]">
+                                    <h1 className=" font-bold text-md">
+                                        {first_name} {last_name}
+                                    </h1>
+                                    <div className="flex">
+                                        <p className=" uppercase text-[#379E37] text-xs font-bold mr-4">
+                                            {level}LVL
+                                        </p>
+                                        <select className="text-xs uppercase font-semibold  text-[#B7802C] border-none focus:outline-none" name="course" id="course">
+                                            <option value="gns-202">gns202</option>
+                                            <option value="vte-202">vte202</option>
+                                            <option value=""></option>
+                                            <option value=""></option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className=" h-[30px] w-[30px] bg-green-700 profile rounded-full mx-1"></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className=' w-full flex flex-col lg:flex-row items-center justify-between'>
+                        <div className=' w-full lg:w-[60%]'>
                             <h1 className=' font-bold text-[#379E37] text-xl mb-4'>Course Code</h1>
                             <div className=' bg-white w-full px-3 flex items-center justify-start py-3 rounded-md mb-3'>
                                 <Search />
                                 <input type="text" className=' h-full bg-transparent px-3 focus:outline-none text-black placeholder:text-black text-sm' placeholder='Search here...' />
                             </div>
                             <div className={`${montserrat.className} text-sm font-thin w-full bg-white h-[250px] overflow-scroll rounded-md py-3 mb-4`}>
-                                {courses.map((course, idx) => {
+                                {courses?.map((course, idx) => {
                                     return (
                                         // @ts-ignore
                                         <h1 onClick={() => updateCourseCode(course)} key={`${course.course}-${idx}`} className='cursor-pointer w-full bg-white py-2 px-10 border-b border-b-slate-300'>{course.course}</h1>
@@ -132,7 +169,7 @@ export default function Page() {
                             </div>
                         </div>
 
-                        <PaymentBlock selectionDetails={selectionDetails}/>
+                        <PaymentBlock selectionDetails={selectionDetails} />
                     </div>
                 </div>
             </FadeInFromBottom>
@@ -141,7 +178,7 @@ export default function Page() {
 }
 
 
-function PaymentBlock({selectionDetails}) {
+function PaymentBlock({ selectionDetails }) {
     const [loading, setLoading] = useState(false);
     const router = useRouter();
 
@@ -150,7 +187,7 @@ function PaymentBlock({selectionDetails}) {
         let specialization = selectionDetails.course_specialization;
         if (course == "") return;
         setLoading(true);
-        let url = await makePayment({course, specialization});
+        let url = await makePayment({ course, specialization });
         if (url) {
             router.push(url);
         } else {
@@ -160,7 +197,7 @@ function PaymentBlock({selectionDetails}) {
 
 
     return (
-        <div className=' w-[40%] h-[100vh] flex flex-col items-center justify-start py-24'>
+        <div className=' w-full lg:w-[40%] min-h-[100vh] flex flex-col items-center justify-start py-24'>
             <div className=' w-full text-center mb-4'>
                 <h1 className=' font-bold text-[#379E37] text-xl mb-4'>Selection Details</h1>
                 <div className=' w-[80%] m-auto py-4 bg-[#98c898] rounded-lg mb-4'>
@@ -190,7 +227,7 @@ function PaymentBlock({selectionDetails}) {
                     </div>
                 </div>
                 <div className=' w-full flex flex-col items-center justify-center'>
-                    <button onClick={handlePaymentClick} type='button' className={"w-[80%] bg-[#379E37] text-white mb-4 py-2 rounded-md"} disabled={loading}>{loading? "loading...":"Make Payment"}</button>
+                    <button onClick={handlePaymentClick} type='button' className={"w-[80%] bg-[#379E37] text-white mb-4 py-2 rounded-md"} disabled={loading}>{loading ? "loading..." : "Make Payment"}</button>
                     <Link href={"/payment"} className=' text-sm text-[#379E37] underline'>Show Payment History</Link>
                 </div>
             </div>
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -20,14 +20,15 @@ import { Avatar } from '@mui/material'
 import { FadeInFromBottom } from '../components/FadeInFromBottom'
 import { Protected } from '@/components/protected'
 import Sidebar from '../components/Sidebar'
+import ResponsiveSidebar from '../components/ResponsiveSidebar'
 
 const page = () => {
   return (
     <Protected>
       <Sidebar />
       <FadeInFromBottom>
-        <div className='  w-full h-[100vh] overflow-hidden pl-[20%] py-2 pr-4'>
-          <div className=' h-[60px] w-full bg-transparent rounded-md flex items-center justify-between p-2 mb-4'>
+        <div className='  w-full h-[100vh] overflow-hidden lg:pl-[20%] py-2 lg:pr-4 px-2 lg:px-0'>
+          <div className=' h-[60px] w-full bg-transparent rounded-md hidden lg:flex items-center justify-between p-2 mb-4'>
             <div className=' flex items-center justify-center bg-white p-2 rounded-md'>
               <CalendarMonth className=' text-[#379E37]' />
               <select name="" id="" className=' mx-4 appearance-none'>
@@ -45,14 +46,51 @@ const page = () => {
               </div>
             </div>
           </div>
+
+          {/* Mobile header */}
+          <div className=" lg:hidden w-[100vw] h-[60px] px-3 flex items-center justify-between">
+            <ResponsiveSidebar />
+
+            <div className=" h-[90%] w-fit bg-white rounded-lg px-2">
+              <div className=" h-full flex items-center justify-center">
+                <div className=" mx-2 bg-[#BFE7BF7A] h-[40px] w-[40px] rounded-md flex items-center justify-center p-2 cursor-pointer">
+                  <Search />
+                </div>
+
+                <Link href={"/notifications/"}>
+                  <Notifications className=" text-[#379E37] mr-3" />
+                </Link>
+
+
+                <div className=" mx-1 h-full flex flex-col items-start justify-center min-w-[100px]">
+                  <h1 className=" font-bold text-md">
+                    Olusanmi Pelumi
+                  </h1>
+                  <div className="flex">
+                    <p className=" uppercase text-[#379E37] text-xs font-bold mr-4">
+                      200LVL
+                    </p>
+                    <select className="text-xs uppercase font-semibold  text-[#B7802C] border-none focus:outline-none" name="course" id="course">
+                      <option value="gns-202">gns202</option>
+                      <option value="vte-202">vte202</option>
+                      <option value=""></option>
+                      <option value=""></option>
+                    </select>
+                  </div>
+                </div>
+                <div className=" h-[30px] w-[30px] bg-green-700 profile rounded-full mx-1"></div>
+              </div>
+            </div>
+          </div>
+
           <h1 className=' font-bold text-[#379E37] text-xl mb-4'>Settings</h1>
           <div className="w-full flex items-start justify-between">
-            <div className=' w-[20%] bg-white h-[250px] rounded-md py-2'>
+            <div className=' w-[20%] hidden lg:block bg-white h-[250px] rounded-md py-2'>
               <div className=' py-2 text-left bg-[#E2FFE2] text-[#379E37] text-sm font-semibold'><h1 className=' flex items-center justify-center'><Person className=' mx-2' /> Profile Settings</h1></div>
             </div>
-            <div className=' w-[78%] h-[600px] bg-white py-10 px-16'>
-              <div className=' w-full flex items-start justify-between mb-6'>
-                <div className=' flex items-start justify-center'>
+            <div className=' w-full lg:w-[78%] lg:h-[600px] bg-white py-10 px-16'>
+              <div className=' w-full flex flex-col lg:flex-row items-start justify-between mb-6'>
+                <div className=' flex flex-col lg:flex-row items-start justify-center'>
                   <div className=' h-[100px] w-[100px] bg-red-800 profile rounded-full'></div>
                   <div className=' mx-4 py-2'>
                     <h1 className=' text-lg font-semibold'>Olusanmi Pelumi</h1>
@@ -131,8 +169,8 @@ const page = () => {
                 </label>
               </div>
 
-              <div className=" w-[80%] flex items-start justify-between p-0 m-0 mb-4">
-                <div className="relative w-[48%] mx-auto mb-6">
+              <div className=" w-[80%] flex flex-col lg:flex-row items-start justify-between p-0 m-0 mb-4">
+                <div className="relative w-full lg:w-[48%] mx-auto mb-6">
                   <input
                     type="text"
                     id="name"
@@ -148,7 +186,7 @@ const page = () => {
                     Matric Number
                   </label>
                 </div>
-                <div className="relative w-[48%] mx-auto mb-6">
+                <div className="relative w-full lg:w-[48%] mx-auto mb-6">
                   <input
                     type="text"
                     id="name"
